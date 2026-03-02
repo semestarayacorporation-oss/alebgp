c3ebc5d48f49a63b534427e3bdec12b1832815d5
semestarayacorporation-oss <semestarayacorporation@gmail.com>
2026-03-02 14:53:54 +0700

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,9 @@ document.addEventListener('DOMContentLoaded', () => {
     const menus = [
         { name: 'Dashboard Ops', hash: '#/dashboard-ops', icon: 'M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6' },
         { name: 'Entri Resi Cash', hash: '#/resi-cash', icon: 'M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z' },
+// Tambahkan di dalam file app.js bagian `const menus = [...]`
+{ name: 'Master Pelanggan', hash: '#/master-customer', icon: 'M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z' },
+{ name: 'Master Vendor', hash: '#/master-vendor', icon: 'M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4' }
     ];
 
     if (user.role === 'admin') {
